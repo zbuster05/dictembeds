496a1cbdd1e239201307067277aa714e5302c6fc
Houjun Liu <hliu@shabang.cf>
2021-04-19 23:22:53 -0700

    "immediately and correctly"

--- scratchpad.py
+++ scratchpad.py
@@ -1,7 +1,7 @@
 # type: ignore
 # pylint: disable=no-member
 
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, AdamW
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, AdamW, get_cosine_schedule_with_warmup
 import torch
 
 from torch.utils.data import DataLoader
@@ -22,7 +22,8 @@ for identifier in tqdm.tqdm(range(45)):
 
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-tokenizer.add_special_tokens({'sep_token': '<|seperator|>', 'pad_token':'<|pad|>'})
+tokenizer.add_special_tokens({'sep_token': '<|seperator|>'})
+tokenizer.pad_token = tokenizer.eos_token
 
 class EnWikiKeywordSentsDataset(torch.utils.data.Dataset):
     def __init__(self, tokenizer, directory="./enwiki_parsed", filebasename="enwiki-parsed_", mod=65536, total=45):
@@ -45,18 +46,15 @@ class EnWikiKeywordSentsDataset(torch.utils.data.Dataset):
         output_tokenized = tokenizer.tokenize(output_string)
         input_tokenized = tokenizer.tokenize(input_string)[:(512 - (len(output_tokenized) + 3))]
 
-        input_truncated = [tokenizer.bos_token]+input_tokenized+[tokenizer.sep_token]
-        output_truncated = [tokenizer.bos_token]+input_tokenized+[tokenizer.sep_token] + output_tokenized[:512-3] + [tokenizer.eos_token]
+        data_truncated = [tokenizer.bos_token]+input_tokenized+[tokenizer.sep_token] + output_tokenized[:512-3] + [tokenizer.eos_token]
 
-        input_padded = input_truncated + [tokenizer.pad_token for _ in range(512-len(input_truncated))]
-        output_padded = output_truncated + [tokenizer.pad_token for _ in range(512-len(output_truncated))]
+        data_padded = data_truncated + [tokenizer.pad_token for _ in range(512-len(data_truncated))]
+    
+        data_encoded = tokenizer.convert_tokens_to_ids(data_padded)
 
-        input_encoded = tokenizer.convert_tokens_to_ids(input_padded)
-        output_encoded = tokenizer.convert_tokens_to_ids(input_padded)
+        mask = [1 for _ in range(len(data_truncated))] + [0 for _ in range(512-len(data_truncated))]
 
-        input_mask = [1 for _ in range(len(input_truncated))] + [0 for _ in range(512-len(input_truncated))]
-
-        return {"input": torch.LongTensor([input_encoded]), "output": torch.LongTensor([output_encoded]), "input_mask": torch.LongTensor([input_mask])}
+        return {"data": torch.LongTensor([data_encoded[:512]]), "mask": torch.LongTensor([mask[:512]])}
 
     def __len__(self):
         return self.mod*self.total
@@ -73,12 +71,13 @@ model.train()
 train_dataset = EnWikiKeywordSentsDataset(tokenizer)
 train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
 
-optim = AdamW([
-        {"params": model.transformer.h.parameters(), "lr": 5e-6},
-        {"params": model.lm_head.parameters(), "lr": 1e-5},
-    ], lr=1e-5)
+# optim = AdamW([
+    # {"params": model.transformer.h.parameters(), "lr": 5e-6},
+        # {"params": model.lm_head.parameters(), "lr": 1e-5},
+    # ], lr=1e-5)
 
-# optim = AdamW(model.parameters(), lr=5e-5)
+optim = AdamW(model.parameters(), lr=3e-5)
+scheduler = get_cosine_schedule_with_warmup(optim, num_warmup_steps = 1000, num_training_steps = 3*len(train_loader))
 
 modelID = str(uuid.uuid4())[-5:]
 
@@ -91,13 +90,14 @@ for epoch in range(3):
     writer = SummaryWriter(f'./training/{modelID}')
     for i, chicken in enumerate(databatched_loader):
         optim.zero_grad()
-        inputs = chicken['input'].to(device)
-        attention_mask = chicken['input_mask'].to(device)
-        outputs = chicken['output'].to(device)
 
-        outputs[outputs==50256] = -100
+        data_batch = chicken['data'].to(device)
+        attention_mask = chicken['mask'].to(device)
+
+        data_labels = torch.clone(data_batch)
+        data_labels[data_labels==50258] = -100
          
-        logits = model(inputs, attention_mask=attention_mask, labels=outputs)
+        logits = model(data_batch, attention_mask=attention_mask, labels=data_labels)
         loss = logits[0]
 
         databatched_loader.set_description(f'{modelID} loss: {loss}')
@@ -105,6 +105,7 @@ for epoch in range(3):
     
         loss.backward()
         optim.step()
+        scheduler.step()
 
         oneAnswer = torch.argmax(logits[1][0][0], dim=1)
         answer_tokens = tokenizer.convert_ids_to_tokens(oneAnswer)
