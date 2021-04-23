50c47b0e8e19934cdafa72ce255a7ce29f3287b7
Houjun Liu <hliu@shabang.cf>
2021-04-22 20:08:31 -0700

    here's a meh pipeline

--- bart_execute.py
+++ bart_execute.py
@@ -7,78 +7,88 @@ import torch
 import uuid
 import tqdm
 import json
+import time
 import os
 
+class Engine:
+    def __init__(self, path:str="./training/bart_enwiki_BASE-6c279:0:400000"):
+        self.tokenizer = BartTokenizer.from_pretrained(path)
+        self.model = BartForConditionalGeneration.from_pretrained(path)
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.model.to(device)
 
-tokenizer = BartTokenizer.from_pretrained("./training/bart_enwiki_BASE-05fcd:0:1000")
+    def __pre_process_sample(self, article:str="", context:str=""):
+        return self.tokenizer.convert_tokens_to_ids(
+                [self.tokenizer.bos_token] + 
+                self.tokenizer.tokenize(article) + 
+                [self.tokenizer.sep_token] + 
+                self.tokenizer.tokenize(context) + 
+                [self.tokenizer.eos_token])
 
-model = BartForConditionalGeneration.from_pretrained("./training/bart_enwiki_BASE-05fcd:0:1000")
+    def generate_syntheses(self, processed_samples:[torch.Tensor]):
+        summary_ids = self.model.generate(processed_samples, num_beams=4, early_stopping=True)
+        return [self.tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids]
 
-device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    def batch_process_samples(self, samples:[[str,str]]):
+        results = []
+        max_length = 0
 
-model.to(device)
+        for sample in samples:
+            sample_encoded = self.__pre_process_sample(sample[0], sample[1])
+            results.append(sample_encoded)
+            max_length = max(max_length, len(sample_encoded))
+
+        for indx, i in enumerate(results):
+            results[indx] = i+[self.tokenizer.pad_token_id for i in range(max_length-len(i))]
+
+        return torch.LongTensor(results)
+
+    def execute(self, samples:[[str,str]]):
+        return self.generate_syntheses(self.batch_process_samples(samples))
 
 
-ARTICLE_TO_SUMMARIZE = """
-Whole segments of genome exchange abruptly as two flu viruses infect the same cell to create a new strand.  There are two mechnisms by which happens — the crossing-over mechnism and genome segment reassortment Self-mixing of ozaki fragments during viral recombination in the ozaki process cause sudden mutations. Two viruses coinfect the same cell, causing cross-talk in swapping segment.
+a = time.time()
+E = Engine()
+b = time.time()
+res = E.execute([
+    ["Retroviruses", 
+    """Viruses that have the ability to intergrate into the chromosomes of the host cell.  
 
-This usually occurs due an error in a polymerase-driven process, where single/groups of nucleotides flip slowly over time due to mistakes in replication. The former is an environment-dependent process, where the latter is able to be modeled as it is due to predictable transcription mistake.
-"""
-inputs = tokenizer([ARTICLE_TO_SUMMARIZE], max_length=512, return_tensors='pt')
+Early Events
 
-summary_ids = model.generate(inputs['input_ids'], num_beams=4, early_stopping=True)
-print([tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids])
+* Viruses is uncoated, and uses an enzyme called reverse transcriptase to turn ssRNA to cDNA, and finally into dsDNA
+* Then, the enzyme integrase threads the viral dsDNA into the cell's nucleaus
+* HIV protease cuts HIV polyproteins into individual parts ready for budding
+
+Late Events
+
+* Proviral region is transcribed slowly whenever ribosome comes across it by the host DNA polymerase II to make viral proteins + replicate the viral genome
+* Components are later exported, assembled, and slowly released through budding
+
+To make this happen, the virus needs...
+
+- Reverse Transcriptase
+    - Transcript RNA to double-stranded RNA
+    - Take double-stranded RNA to turn into DNA
+- Integrase
+    - Force insert the DNA into the genome of the host cell
+
+And because of the fact that viral DNA is now in cellular DNA, these viruses' DNAs are hard to get rid of.
+
+And this is why we can't cure HIV.
+
+Virus, in this case, spread through cell duplication
+
+* Proviral region on the DNA, every time the ribosome comes across it, makes a new viron
+* These components are then assembled, sent, etc. as usual
+* Because of the fact that the ribosome needs to, well, come across the bit of DNA for this to work, the virons are made slowly by "trickling out. """],
+    ["Mitosis",
+    """* Chromesomes line up in equator
+* Each chromesome has two chromatid exactly the same
+* Microtubials to pull chromesomes appart connected to kinecore, a joint in the chromatid
+* Kinetore senses tension, and when it is correct, molecules are sent down the microtubials to send a split signal"""]
+
+])
+c = time.time()
 breakpoint()
-
-# model.resize_token_embeddings(len(tokenizer))
-
-# model.train()
-
-# train_dataset = EnWikiKeywordSentsDataset(tokenizer)
-# train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
-
-# optim = AdamW([
-    # {"params": model.transformer.h.parameters(), "lr": 5e-6},
-        # {"params": model.lm_head.parameters(), "lr": 1e-5},
-    # ], lr=1e-5)
-
-# optim = AdamW(model.parameters(), lr=3e-5)
-# scheduler = get_cosine_schedule_with_warmup(optim, num_warmup_steps = 1000, num_training_steps = 3*len(train_loader))
-
-# modelID = str(uuid.uuid4())[-5:]
-
-# model.save_pretrained(f"./training/bart_enwiki_BASE-{modelID}")
-# # https://huggingface.co/transformers/custom_datasets.html?highlight=fine%20tuning
-# # model.resize_token_embeddings(len(tokenizer))
-# for epoch in range(3):
-    # databatched_loader = tqdm.tqdm(train_loader)
-
-    # writer = SummaryWriter(f'./training/{modelID}')
-    # for i, chicken in enumerate(databatched_loader):
-        # optim.zero_grad()
-
-        # input_data = chicken['input_data'].to(device)
-        # output_data = chicken['output_data'].to(device)
-        # attention_mask = chicken['input_mask'].to(device)
-
-        # result = model(input_data, attention_mask=attention_mask, labels=output_data)
-        # logits = result["logits"]
-        # loss = result["loss"]
-
-        # databatched_loader.set_description(f'{modelID} loss: {loss}')
-        # databatched_loader.refresh()
-    
-        # loss.backward()
-        # optim.step()
-        # scheduler.step()
-
-        # oneAnswer = torch.argmax(logits[0], dim=1)
-        # answer_tokens = tokenizer.convert_ids_to_tokens(oneAnswer)
-        # answer = tokenizer.convert_tokens_to_string(answer_tokens)
-        
-        # writer.add_scalar('Train/loss', loss.item(), i+(epoch*len(databatched_loader)))
-        # writer.add_text('Train/sample', answer, i+(epoch*len(databatched_loader)))
-    
-    # model.save_pretrained(f"./training/bart_enwiki_{epoch}-{modelID}")
-
 
