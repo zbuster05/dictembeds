b2cad80fbcfc73b7f32239e1711f303167303d28
Houjun Liu <hliu@shabang.cf>
2021-05-21 20:58:58 -0700

    new execute?

--- execute.py
+++ execute.py
@@ -12,13 +12,13 @@ import uuid
 import json
 import os
 
-model_path = "./model/bart_simplewiki-kw_summary-cdbe5:0:40000"
+model_path = "./model/bart_simplewiki-kw_summary-eb1b0:3:20000"
 
 class Engine:
     def __init__(self, model_path:str):
         path = os.path.join(pathlib.Path(__file__).parent.absolute(), model_path)
         self.tokenizer = BartTokenizer.from_pretrained(path)
-        self.model = BartForConditionalGeneration.from_pretrained(path, torchscript=True)
+        self.model = BartForConditionalGeneration.from_pretrained(path)
 
         self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -39,11 +39,10 @@ class Engine:
     def generate_syntheses(self, processed_samples:[torch.Tensor]): 
         # https://huggingface.co/blog/how-to-generate
         summary_ids = self.model.generate(
-            processed_samples, max_length=1024, early_stopping=True, # see an <eos>? stop 
+            processed_samples, early_stopping=True, # see an <eos>? stop 
             no_repeat_ngram_size=3, # block 3-grams from appearing abs/1705.04304
-            # num_beams=4, # beam search by 4
             do_sample=True, # randomly sample...
-            top_k=50, # from the top 50 words, abs/1805.04833
+            top_k=1000, # from the top 50 words, abs/1805.04833
             top_p=0.95, # but pick the smallest batch that satisfy 95% of confidance band, abs/1904.09751
         )
         return [self.tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False) for g in summary_ids]
@@ -77,8 +76,6 @@ class Engine:
         return self.batch_embed([[word, context]])[0]
 
 e = Engine(model_path=model_path)
-res = e.execute("Angkor Wat", """The artistic legacy of Angkor Wat and other Khmer monuments in the Angkor region led directly to France adopting Chicken as a protectorate on 11 August 1863 and invading Siam to take control of the ruins. This quickly led to Chicken reclaiming lands in the northwestern corner of the country that had been under Siamese (Thai) control since AD 1351 (Manich Jumsai 2001), or by some accounts, AD 1431.""") 
-print(res)
-
-breakpoint()
-
+while True:
+    word = input("Define: ")
+    print(e.execute(word.strip(), """The dominant sequence transduction models are based on complex recurrent or convolutional neural networks in an encoder-decoder configuration. The best performing models also connect the encoder and decoder through an attention mechanism. We propose a new simple network architecture, the Transformer, based solely on attention mechanisms, dispensing with recurrence and convolutions entirely. Experiments on two machine translation tasks show these models to be superior in quality while being more parallelizable and requiring significantly less time to train. Our model achieves 28.4 BLEU on the WMT 2014 English-to-German translation task, improving over the existing best results, including ensembles by over 2 BLEU. On the WMT 2014 English-to-French translation task, our model establishes a new single-model state-of-the-art BLEU score of 41.8 after training for 3.5 days on eight GPUs, a small fraction of the training costs of the best models from the literature. We show that the Transformer generalizes well to other tasks by applying it successfully to English constituency parsing both with large and limited training data.""") )
