8d7d312d2871e47b27bcdb84e01b107e8b7969e6
Houjun Liu <hliu@shabang.cf>
2021-08-09 10:12:19 -0700

    interesting and text from gutenberg

--- glossarization.py
+++ glossarization.py
@@ -10,72 +10,45 @@ import re
 
 # model_path = "./training/bart_enwiki-kw_summary-12944:ROUTINE::0:30000"
 # model_path = "./training/bart_enwiki-kw_summary-a2fc9:B_VAL::0:24900:0.8616854640095484"
-model_path = "./training/bart_enwiki-kw_summary-12944:ROUTINE::0:30000"
+# model_path = "./training/bart_enwiki-kw_summary-12944:ROUTINE::0:30000"
 # model_path = "./training/bart_enwiki-kw_summary-a5029:ROUTINE::0:30000"
 # model_path = "./training/bart_enwiki-kw_summary-cf8cd:ROUTINE::0:20000"
+model_path = "./training/bart_enwiki-kw_summary-3dee1:B_VAL::0:47200:1.8260153889656068"
 
-TOP_TFIDF_REMAIN = 50 # number of top words of each doc to include
-TOP_CONSIDER_THRESHOLD = 4 # count of top across documents before consideration
+TFIDF_FINAL_INCLUDE = 50 # "important" words to include
 
-with open("./colonialism.txt", "r") as data:
+with open("./textbook.txt", "r") as data:
     data_text = data.read()
 
-
-paragraphs = list(filter(lambda x:(x!='' and len(x)>25), data_text.lower().split("\n")))
-tokenized_paragraphs = [word_tokenize(i) for i in paragraphs]
+documents = [i.strip() for i in list(filter(lambda x:(x!='' and len(x)>1000), re.sub("_", "", re.sub("\n", " ", data_text)).split("====")))]
+tokenized_documents = [re.findall("[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+",i) for i in documents]
 
 df = defaultdict(int)
 tfs = []
-for doc in tokenized_paragraphs:
+for doc in tokenized_documents:
     d = defaultdict(int)
     for word in doc:
         if word not in d:
             df[word] += 1
         d[word] += 1
-    tfs.append({i:j/len(doc) for i,j in d.items()})
-idf = {i:math.log(len(paragraphs)/j, math.e) for i,j in df.items()}
+    tfs.append({i:math.log(1+j/len(doc), 2) for i,j in d.items()})
+idf = {i:math.log(len(documents)/j, 2) for i,j in df.items()}
 
 tfidf_count = defaultdict(int)
+tfidf_sum = defaultdict(int)
+
 for i in tfs:
     res = sorted({k:j*idf[k] for k,j in i.items()}.items(), key=lambda x:x[1])
-    for j in res[-TOP_TFIDF_REMAIN:]:
+    for j in res:
+        tfidf_sum[j[0]] += j[1]
         tfidf_count[j[0]] += 1
 
-tfidf_count = sorted(tfidf_count.items(), key=lambda i:i[1])
+tfidf = {i:tfidf_sum[i]/tfidf_count[i] for i in tfidf_count.keys()}
+
+tfidf_sorted = sorted(tfidf.items(), key=lambda i:i[1])
+idf_sorted = sorted(idf.items(), key=lambda i:i[1])
+
+word_list = list(filter(lambda x:len(x)>3, set([i[0] for i in tfidf_sorted[-TFIDF_FINAL_INCLUDE:]])))
 
 breakpoint()
-
-
-
-# e = Engine(model_path=model_path)
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-    # try:
-        # title = request.json["title"]
-        # context = request.json["context"]
-    # except (KeyError, TypeError):
-        # return jsonify({"code": "bad_request", "response": "Bad request. Missing key(s) title, context.", "payload": ""}), 400
-
-    # params = request.json.get("params")
-
-    # try: 
-        # if params:
-            # result = e.execute(title.strip(), 
-                                # re.sub(r"\n", "", context.strip()), 
-                                # num_beams=int(params["num_beams"]), 
-                                # min_length=int(params["min_length"]), 
-                                # no_repeat_ngram_size=int(params["no_repeat_ngram_size"]))
-        # else:
-            # result = e.execute(title.strip(), re.sub(r"\n", "", context.strip()))
-    # except ValueError:
-        # return jsonify({"code": "size_overflow", "response": f"Size overflow. The context string is too long and should be less than {e.tokenizer.model_max_length} tokens.", "payload": e.tokenizer.model_max_length}), 418
-    # except (KeyError, TypeError):
-        # return jsonify({"code": "bad_request", "response": "Bad request. Missing few of key(s) num_beams,  min_length or no_repeat_ngram_size in params.", "payload": ""}), 400
-
-    # return {"code": "success", "response": result}, 200
-
-# if __name__ == "__main__":
-    # app.run()
-
 
