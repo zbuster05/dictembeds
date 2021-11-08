6fb1d93d1995aedce3537b7cbf8843b17461ff36
zbuster05 <zakary.saya@gmail.com>
2021-11-07 16:32:58 -0800

    Might work

--- wordnet/wordnet.py
+++ wordnet/wordnet.py
@@ -2,10 +2,12 @@ import nltk
 from nltk.corpus import wordnet as wn
 
 def get_word_definition(word):
+    definitions = []
     try:
-        definition = wn.synsets(word+".n.01").definition()
+        for definition in wn.synsets(word)
+            definitions.append(definition.definition())
     except(nltk.corpus.reader.wordnet.WordNetError):
         print(word + " was skipped because it doesn't bloody exist")
         definition = None
-    return definition
+    return definitions
 
