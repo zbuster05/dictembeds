208baf7939c42eb567ad715dbe1dd6611ac4f697
jemoka <hliu@shabang.cf>
2021-11-07 16:08:56 -0800

    finishing validation code to hook onto zach

--- validate_wordnet.py
+++ validate_wordnet.py
@@ -1,6 +1,7 @@
 import json
 from tqdm import tqdm
 from execute import Engine
+from rouge_score import rouge_scorer
 
 MODEL_PATH = "./training/bart_enwiki-kw_summary-2d8df:ROUTINE::1:10000"
 e = Engine(model_path=MODEL_PATH)
@@ -8,6 +9,15 @@ e = Engine(model_path=MODEL_PATH)
 with open("./validation_wordnet/validation_subset.json", "r") as df:
     raw_validation_data = json.load(df)
 
-len(raw_validation_data)
+scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
+pairs = [[(i["title"], i["context"]), i["target"]] for i in raw_validation_data]
 
+collected_pairs = []
+for sample in pairs:
+    word = sample[0][0]
+    #definition = CallZachsWonderfulFunction(word.lower())
+    definition = ''
+
+    if definition:
+        collected_pairs.append([sample[0], (sample[1], definition)])
 
