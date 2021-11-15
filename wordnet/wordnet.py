ac905725af383579a388d70ad407ee75d9306f06
zbuster05 <zakary.saya@gmail.com>
2021-11-14 23:02:49 -0800

    Added things

---
+++ wordnet/wordnet.py
@@ -0,0 +1,12 @@
+import nltk
+from nltk.corpus import wordnet as wn
+
+def get_word_definition(word):
+    definitions = []
+    try:
+        for definition in wn.synsets(word)
+            definitions.append(definition.definition())
+    except(nltk.corpus.reader.wordnet.WordNetError):
+        print(word + " was skipped because it doesn't bloody exist")
+    return definitions
+
